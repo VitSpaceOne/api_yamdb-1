d7caedcc53504b7283c6a5eee03577c2dfa47a33
Бурич Богдан <78378971+mollador@users.noreply.github.com>
2022-06-02 05:06:22 +0400

    refactor permissions

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,40 +1,21 @@
 from rest_framework import permissions
 
 
-class BaseUserPermission(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
+class IsSuperuser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role in self.allowed_roles
-            or request.user.is_superuser
-        )
+        return request.user.is_superuser
 
 
-class AdminOrReadOnly(BaseUserPermission):
-
-    allowed_roles = ('admin',)
-
-
-class AdminOrModeratorOrReadOnly(BaseUserPermission):
-
-    allowed_roles = ('admin', 'moderator')
-
-
-class AdminOrModeratorOrOwnerOrReadOnly(BaseUserPermission):
-
-    allowed_roles = ('admin', 'moderator')
-
+class IsAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user == obj.author
-            or request.user.role in self.allowed_roles
-            or request.user.is_superuser
-        )
+        return request.user.role == 'admin'
+
+
+class IsModertor(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == 'moderator'
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
