5505286dd05c64f1186e2210af5c2add64cad17e
Сергей Колобов <seryonka88@gmail.com>
2022-06-04 17:49:43 +0300

    api_3

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import serializers
 
 from reviews.models import (
@@ -7,8 +8,6 @@ from reviews.models import (
     Review,
     Comment,
 )
-
-from users.models import User
 
 
 class CategoriesSerializer(serializers.ModelSerializer):
@@ -26,21 +25,67 @@ class GenresSerializer(serializers.ModelSerializer):
 
 
 class TitlesSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        many=False,
+        queryset=Category.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        many=True,
+        required=False,
+        queryset=Genre.objects.all()
+    )
 
     class Meta:
         model = Title
         fields = '__all__'
 
+    def validate_year(self, value):
+        current_year = datetime.date.today().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                'Год выпуска не может быть больше текущего'
+            )
+        return value
+
 
 class ReviewsSerializer(serializers.ModelSerializer):
+    auhtor = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
         model = Review
         fields = '__all__'
+        read_only_fields = 'title'
+
+    def validate(self, data):
+        if self.context['request'].method == 'POST':
+            title_id = (
+                self.context['request'].parser_context['kwargs']['title_id']
+            )
+            user = self.context['request'].user
+            if user.reviews.filter(title_id=title_id).exist():
+                raise serializers.ValidationError(
+                    'Возможно оставить только один отзыв на произведение'
+                )
+        return data
+
+    def validate_score(self, value):
+        if 0 >= value >= 10:
+            raise serializers.ValidationError(
+                'Оценка'
+            )
+        return value
 
 
 class CommentsSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True)
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ('id', 'author', 'text', 'pub_date')
