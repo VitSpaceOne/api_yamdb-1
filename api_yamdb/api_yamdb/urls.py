0dc90bc8e319edaca5d4afd24ae9dbbb8091e549
Bogdan <bogdan.buritch@yandex.ru>
2022-08-04 21:11:38 +0400

    PEP8 sorting

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,13 +1,10 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
 from django.views.generic import TemplateView
-
-
-from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
